d5c63e5f457c572c5937c98bd7e3afac0805ecda
Harry Bovik <hbovik64@gmail.com>
2014-01-13 18:25:38 -0500

    added

---
+++ a.js
@@ -0,0 +1,14 @@
+var d = new webkitAudioContext();
+node = d.createScriptProcessor(2048,1,1);
+node.onaudioprocess = function (e) {
+  var output = e.outputBuffer.getChannelData(0);
+  for (var i = 0; i < output.length; i++) {
+    output[i] = Math.random();
+    // Math.random() sends random numbers, but you can make
+    // that be anything you want
+  }
+}
+node.connect(d.destination);
+$(document).on('click', function() {
+  node.disconnect(0);
+});
